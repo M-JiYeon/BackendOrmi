fd72fdd2c31e43e9a162f9c512cf84ae35792a48
M-JiYeon <44043858+M-JiYeon@users.noreply.github.com>
2023-06-23 23:58:19 +0900

    2023-06-23 django

--- Django/myapp/blog/views.py
+++ Django/myapp/blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
-from django.views.generic import ListView, CreateView, DeleteView
-from .models import Post
-from .forms import PostForm
-from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from .models import Post, Comment, HashTag
+from .forms import PostForm, CommentForm, HashTagForm
+from django.urls import reverse_lazy, reverse
 
 # Create your views here.
 # def index(request): # request 가 get인지 post인지
@@ -14,7 +14,7 @@ from django.urls import reverse_lazy
 #     # 에러, 예외처리
 #     return HttpResponse('No!!!')
 
-
+### Post
 class Index(View):
     def get(self, request):
         # return HttpResponse('Index page GET class')
@@ -27,7 +27,7 @@ class Index(View):
         context = {
             "posts": post_objs
         }
-        # print(post_objs) QuerySet<[1, 2, 3, 4 ,5]>
+        # print(post_objs) QuerySet<[post 1, 2, 3, 4 ,5]>
         return render(request, 'blog/board.html', context)
 
 
@@ -62,7 +62,118 @@ class Write(CreateView):
     success_url = reverse_lazy('blog:list') # 성공시 보내줄 url
 
 
-class Detail(DeleteView):
+class Detail(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
+
+
+class Update(UpdateView):
+    model =  Post
+    template_name = 'blog/post_edit.html'
+    fields = ['title', 'content']
+    # success_url = reverse_lazy('blog:list')
+
+    # initial 기능 사용 -> form에 값을 미리 넣어주기 위해서
+    def get_initial(self):
+        initial = super().get_initial() # UpdateView(generic view)에서 제공하는 initial(딕셔너리)
+        post = self.get_object() # pk 기반으로 객체를 가져옴
+        initial['title'] = post.title
+        initial['content'] = post.content
+        return initial
+    
+    def get_success_url(self):
+        post = self.get_object() # pk 기반으로 현재 객체 가져오기
+        return reverse('blog:detail', kwargs={'pk': post.pk})
+
+
+class Delete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:list')
+
+
+class DetailView(View):
+    def get(self, request, pk): # post_id: 데이터베이스 post_id 테이블 이름 사용하고 싶어서
+        # list -> object 상세 페이지 -> 상세 페이지 하나의 내용
+        # pk 값을 왔다갔다, 하나의 인자
+
+        # 데이터베이스 방문
+        # 해당 글
+        # 쿼리문. 장고 ORM (pk: 무조건 pk로 작성해야한다.)
+        post = Post.objects.get(pk=pk)
+        # 댓글
+        comments = Comment.objects.filter(post=post)
+        # 해시태그
+        hashtags = HashTag.objects.filter(post=post)
+        
+        # 댓글 Form
+        comment_form = CommentForm()
+        
+        # 태그 Form
+        hashtag_form = HashTagForm()
+        
+        context = {
+            'post': post,
+            'comments': comments,
+            'hashtags': hashtags,
+            'comment_form': comment_form,
+            'hashtag_form': hashtag_form,
+        }
+
+        return render(request, 'blog/post_detail.html', context)
+
+
+### Comment
+class CommentWrite(View):
+    # def get(self, request):
+    #     pass
+    def post(self, request, pk): # pk = post_id
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            # 사용자에게 댓글 내용을 받아옴
+            content = form.cleaned_data['content']
+            # 해당 아이디에 해당하는 글 불러옴
+            post = Post.objects.get(pk=pk)
+            # 댓글 객체 생성,, create 메서드를 사용할 때는 save 필요 없음
+            comment = Comment.objects.create(post=post, content=content)
+            # comment = Comment(post=post) -> comment.save()
+            return redirect('blog:detail', pk=pk)
+
+
+class CommentDelete(View):
+    def post(self, request, pk): # pk = comment_id
+        # 지울 객체를 찾아야 한다 -> 댓글 객체
+        comment = Comment.objects.get(pk=pk)
+        # 상세페이지로 돌아가기
+        post_id = comment.post.id
+        # 삭제
+        comment.delete()
+        
+        return redirect('blog:detail', pk=post_id)
+
+
+### Hashtag
+class HashTagWrite(View):
+    def post(self, request, pk): # pk = post_id
+        form = HashTagForm(request.POST)
+        if form.is_valid():
+            # 사용자에게 태그 내용을 받아옴
+            name = form.cleaned_data['name']
+            # 해당 아이디에 해당하는 글 불러옴
+            post = Post.objects.get(pk=pk)
+            # 태그 객체 생성,, create 메서드를 사용할 때는 save 필요 없음
+            hashtag = HashTag.objects.create(post=post, name=name)
+            # comment = Comment(post=post) -> comment.save()
+            return redirect('blog:detail', pk=pk)
+
+
+class HashTagDelete(View):
+    def post(self, request, pk): # pk = hashtag_id
+        # 해시태그 불러오기
+        hashtag = HashTag.objects.get(pk=pk)
+        # 포스트의 pk(id) 값
+        hashtag_id = hashtag.post.id
+        # 해시태그삭제
+        hashtag.delete()
+        # 응답
+        return redirect('blog:detail', pk=hashtag_id)
